3808e7f83d99593c5b4217eff7adc73b5651b8f3
cabub <kingcabub@gmail.com>
2019-03-25 02:58:57 -0500

    layed groundwork for submitting rsvps

---
+++ rsvp-submit.php
@@ -0,0 +1,14 @@
+<?php
+    $config = include('config.php');
+    
+    
+    $to = $config['email_to'];
+    $subject = $config['email_subject'];
+    $message = json_encode($_POST);
+    $headers = 'From: ' . $config['email_from'] . "\r\n" .
+        'Reply-To: ' . $config['email_from'] . "\r\n" .
+        'X-Mailer: ' . $config['email_agent'];
+
+    mail($to, $subject, $message, $headers);
+    echo 'submitted';
+?> 
