eee5acd6b14f6f13517ae01dba38a3ac05bd7bc3
cabub <kingcabub@gmail.com>
2019-03-25 00:25:55 -0500

    set up php config

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    return array(
+        'email_from' => 'example@example.com',
+        'email_to' => 'example@example.com',
+        'email_subject' => 'RSVP',
+        'email_agent' => 'X-Mailer: PHP/' . phpversion()
+    )
+?>
