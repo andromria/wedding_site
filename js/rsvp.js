e5146d6abefad3398f0b1ebd89894a1e94a6027a
cabub <cabub@cabub.me>
2019-04-02 13:01:04 -0500

    reinvented the flipclock

--- js/rsvp.js
+++ js/rsvp.js
@@ -57,14 +57,66 @@ $(document).ready(function () {
           $('div.addl-fields').hide();
         }
     });
-    var deadline = (Date.parse("Sat Jun 01 2019 00:00:00 GMT-0500 (Central Daylight Time)") - Date.now()) / 1000;
-    if (deadline > 0) {
-        $('#clock').FlipClock(deadline, {
-            countdown: true,
-            clockFace: 'DailyCounter',
-        });
-    } else {
-        $('#clock').hide();
-        $('#rsvp-form').hide();
+    var labels = ['weeks', 'days', 'hours', 'minutes', 'seconds'],
+      deadline = Date.parse("Sat Jun 01 2019 00:00:00 GMT-0500 (Central Daylight Time)"),
+      template = _.template($('#flip-clock-template').html()),
+      currDate = '00:00:00:00:00',
+      nextDate = '00:00:00:00:00',
+      parser = /([0-9]{2})/gi,
+      $example = $('#flip-clock');
+    // Parse countdown string to an object
+    function strfobj(str) {
+      var parsed = str.match(parser),
+        obj = {};
+      labels.forEach(function(label, i) {
+        obj[label] = parsed[i]
+      });
+      return obj;
     }
+    // Return the time components that diffs
+    function diff(obj1, obj2) {
+      var diff = [];
+      labels.forEach(function(key) {
+        if (obj1[key] !== obj2[key]) {
+          diff.push(key);
+        }
+      });
+      return diff;
+    }
+    // Build the layout
+    var initData = strfobj(currDate);
+    labels.forEach(function(label, i) {
+      $example.append(template({
+        curr: initData[label],
+        next: initData[label],
+        label: label
+      }));
+    });
+    // Starts the countdown
+    $example.countdown(deadline, function(event) {
+      var newDate = event.strftime('%w:%d:%H:%M:%S'),
+        data;
+      if (newDate !== nextDate) {
+        currDate = nextDate;
+        nextDate = newDate;
+        // Setup the data
+        data = {
+          'curr': strfobj(currDate),
+          'next': strfobj(nextDate)
+        };
+        // Apply the new values to each node that changed
+        diff(data.curr, data.next).forEach(function(label) {
+          var selector = '.%s'.replace(/%s/, label),
+              $node = $example.find(selector);
+          // Update the node
+          $node.removeClass('flip');
+          $node.find('.curr').text(data.curr[label]);
+          $node.find('.next').text(data.next[label]);
+          // Wait for a repaint to then flip
+          _.delay(function($node) {
+            $node.addClass('flip');
+          }, 50, $node);
+        });
+      }
+    });
 });
